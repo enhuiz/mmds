20d33651a6ec002ca70177d3d4fd8792c76ffc7b
enhuiz <niuzhe.nz@outlook.com>
2021-10-08 00:26:39 +0800

    add npz ts

--- mmds/modalities/npz.py
+++ mmds/modalities/npz.py
@@ -2,10 +2,21 @@ import attr
 import numpy as np
 
 from .modality import Modality
+from .ts import TimeSeriesModality
 
 
 @attr.define
-class NpzModality(Modality):
+class NpzTrait:
     def _load_impl(self):
         assert self.path, f"got {self.path}."
         return np.load(self.path)["arr_0"]
+
+
+@attr.define
+class NpzModality(NpzTrait, Modality):
+    pass
+
+
+@attr.define
+class NpzTimeSeriesModality(NpzTrait, TimeSeriesModality):
+    pass
