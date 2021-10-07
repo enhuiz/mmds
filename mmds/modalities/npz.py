7a3cf05fe60eedf26cca67e3f4f0a1152303b80e
enhuiz <niuzhe.nz@outlook.com>
2021-10-07 19:36:12 +0800

    fix

--- mmds/modalities/npz.py
+++ mmds/modalities/npz.py
@@ -6,7 +6,6 @@ from .modality import Modality
 
 @attr.define
 class NpzModality(Modality):
-    @property
     def _load_impl(self):
         assert self.path, f"got {self.path}."
         return np.load(self.path)["arr_0"]
