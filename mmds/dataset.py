77d8b65d6b0841501bf0d35fbdecfa3203096bb9
enhuiz <niuzhe.nz@outlook.com>
2021-10-15 19:11:10 +0800

    flexible base class

--- mmds/dataset.py
+++ mmds/dataset.py
@@ -1,21 +1,14 @@
-from .sample import MultimodalSample
-
-
 class MultimodalDataset:
-    Sample = MultimodalSample
-
-    def __init__(self, ids, modality_factories):
+    def __init__(self, samples, modality_factories):
         super().__init__()
-        self.ids = ids
+        self.samples = samples
         self.modality_factories = modality_factories
-        self.samples = self.create_samples()
+        self._register_modalites_to_samples()
 
-    def create_samples(self):
-        samples = [self.Sample(id) for id in self.ids]
-        for sample in samples:
+    def _register_modalites_to_samples(self):
+        for sample in self.samples:
             for modality_factory in self.modality_factories:
                 sample.add_modality(modality_factory)
-        return samples
 
     def __getitem__(self, index):
         return self.samples[index].fetch()
