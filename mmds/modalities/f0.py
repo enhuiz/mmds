2e6913987aa56276f56e47979e3d463d697ff38a
enhuiz <niuzhe.nz@outlook.com>
2021-11-05 00:27:57 +0800

    mel_fn -> spec_fn

--- mmds/modalities/f0.py
+++ mmds/modalities/f0.py
@@ -1,15 +1,16 @@
 import attr
 import numpy as np
+from typing import Union
 
+from ..utils.spectrogram import Spectrogram, MelSpectrogram
 from ..utils.yin import compute_yin
-from ..utils.spectrogram import LogMelSpectrogram
 from .ts import TimeSeriesModality
 from .traits import CalculableModalityTrait
 
 
 @attr.define
 class F0Modality(CalculableModalityTrait, TimeSeriesModality):
-    mel_fn: LogMelSpectrogram
+    spec_fn: Union[Spectrogram, MelSpectrogram]
     f0_min: float = 80
     f0_max: float = 880
     harmo_thresh: float = 0.25
@@ -17,14 +18,14 @@ class F0Modality(CalculableModalityTrait, TimeSeriesModality):
 
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
-        self.sample_rate = self.mel_fn.rate
+        self.sample_rate = self.spec_fn.rate
 
     @property
     def kwargs(self):
         return {
-            "sr": self.mel_fn.sample_rate,
-            "w_len": self.mel_fn.win_length,
-            "w_step": self.mel_fn.hop_length,
+            "sr": self.spec_fn.sample_rate,
+            "w_len": self.spec_fn.win_length,
+            "w_step": self.spec_fn.hop_length,
             "f0_min": self.f0_min,
             "f0_max": self.f0_max,
             "harmo_thresh": self.harmo_thresh,
@@ -40,7 +41,7 @@ class F0Modality(CalculableModalityTrait, TimeSeriesModality):
         f0 = []
         for wav_i in wav.transpose():
             f0_i, _, _, _ = compute_yin(wav_i, **self.kwargs)
-            pad = int((self.mel_fn.win_length / self.mel_fn.hop_length) / 2)
+            pad = int((self.spec_fn.win_length / self.spec_fn.hop_length) / 2)
             f0_i = [0.0] * pad + f0_i + [0.0] * pad
             f0_i = np.array(f0_i, dtype=np.float32)
             f0.append(f0_i)
