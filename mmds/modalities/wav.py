6efe9daefcbb0cc8ed9a10bfdefdf8f7a84b2e9b
enhuiz <niuzhe.nz@outlook.com>
2022-01-08 00:09:09 +0800

    add noise reduce for wav, fix multi channel normalize

--- mmds/modalities/wav.py
+++ mmds/modalities/wav.py
@@ -1,11 +1,18 @@
 import attr
 import numpy as np
-from mmds.exceptions import PackageNotFoundError
+from typing import Optional
 
 try:
     import librosa
+    from noisereduce import reduce_noise
 except:
-    raise PackageNotFoundError("librosa", by="the wav modality")
+    from ..exceptions import PackageNotFoundError
+
+    raise PackageNotFoundError(
+        "librosa",
+        "noisereduce",
+        by="the wav modality",
+    )
 
 from .ts import TimeSeriesModality
 
@@ -13,6 +20,22 @@ from .ts import TimeSeriesModality
 @attr.define
 class WavModality(TimeSeriesModality):
     normalize: bool = True
+    noise_reduce_kwargs: Optional[dict] = None
+
+    def _may_reduce_noise(self, wav):
+        """
+        Args:
+            wav: (t c)
+        """
+        kwargs = self.noise_reduce_kwargs
+
+        if kwargs is None:
+            # do nothing
+            return wav
+
+        wav = reduce_noise(wav.T, sr=self.sample_rate, **kwargs).T
+
+        return wav
 
     def _load_impl(self):
         """
@@ -31,6 +54,8 @@ class WavModality(TimeSeriesModality):
         wav = wav.astype(np.float32)
 
         if self.normalize:
-            wav = wav / np.abs(wav).max()
+            wav = wav / np.max(np.abs(wav), axis=0, keepdims=True)
+
+        wav = self._may_reduce_noise(wav)
 
         return wav
